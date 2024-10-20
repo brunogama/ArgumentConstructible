9f138248cb6251cd1de508faff6fee16818b1369
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-10-20 19:48:39 -0300

    Updated tests

--- Tests/VariadicArgumentConstructableTests/ArgumentConstructibleTests.swift
+++ Tests/VariadicArgumentConstructableTests/ArgumentConstructibleTests.swift
@@ -3,20 +3,21 @@ import Testing
 
 @Suite()
 struct ArgumentConstructibleTests {
-    struct Author: VariadicArgumentConstructable {
-        let name: String
-        let birthYear: Int
-        
-        typealias ArgumentTypes = (name: String, birthYear: Int)
-        
-        static func construct<each T>(_ args: repeat each T) throws -> Self {
-            let (name, birthYear) = try unpack(repeat each args)
-            return Author(name: name, birthYear: birthYear)
-        }
-    }
     
     @Test
     func validTypeCreation() throws {
+        struct Author: VariadicArgumentConstructable {
+            let name: String
+            let birthYear: Int
+            
+            typealias ArgumentTypes = (name: String, birthYear: Int)
+            
+            static func construct<each T>(_ args: repeat each T) throws -> Self {
+                let (name, birthYear) = try unpack(repeat each args)
+                return Author(name: name, birthYear: birthYear)
+            }
+        }
+        
         let name = "John Doe"
         let birthYear = 1980
         let author = try Author.construct(name, birthYear)
@@ -24,8 +25,20 @@ struct ArgumentConstructibleTests {
         #expect(author.birthYear == birthYear)
     }
     
-    @Test
-    func validTInvalidTypeCreation() async throws {
+    @Test("Test invalid parameter passed using ArgumentType with labeled tuples")
+    func testInvalidParameterPassedUsingArgumentTypesWithLabeledTuplesAndExpectedErrorMessage() async throws {
+        struct Author: VariadicArgumentConstructable {
+            let name: String
+            let birthYear: Int
+            
+            typealias ArgumentTypes = (name: String, birthYear: Int)
+            
+            static func construct<each T>(_ args: repeat each T) throws -> Self {
+                let (name, birthYear) = try unpack(repeat each args)
+                return Author(name: name, birthYear: birthYear)
+            }
+        }
+        
         #expect {
             try Author.construct(1, true)
         } throws: { error in
@@ -34,6 +47,33 @@ struct ArgumentConstructibleTests {
             }
             
             let expectedMessage = "Invalid argument types. Expected (name: String, birthYear: Int), but got (Int, Bool). Verify if typealias of ArgumentTypes is a tuple corresponding to the values that need to be unpacked."
+            
+            return error.description == expectedMessage
+        }
+    }
+    
+    @Test("Test invalid parameter passed to the construct ArgumentType with not labeled tuples and expected error message")
+    func testInvalidParameterPassedUsingArgumentTypesWithTuplesWithNoLabelAndExpectedErrorMessage() async throws {
+        struct Author: VariadicArgumentConstructable {
+            let name: String
+            let birthYear: Int
+            
+            typealias ArgumentTypes = (String, Int)
+            
+            static func construct<each T>(_ args: repeat each T) throws -> Self {
+                let (name, birthYear) = try unpack(repeat each args)
+                return Author(name: name, birthYear: birthYear)
+            }
+        }
+        
+        #expect {
+            try Author.construct(1, true)
+        } throws: { error in
+            guard let error = error as? ArgumentConstructionError else {
+                return false
+            }
+            
+            let expectedMessage = "Invalid argument types. Expected (String, Int), but got (Int, Bool). Verify if typealias of ArgumentTypes is a tuple corresponding to the values that need to be unpacked."
             
             return error.description == expectedMessage
         }
